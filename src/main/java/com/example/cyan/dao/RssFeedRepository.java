c10438965ed7164ed9a95e7a27773009bb15179a
Sylvester <sylvesterterdoo@gmail.com>
2020-12-26 21:48:55 +0100

    [ RssFeedRepository ] added crud respository

---
+++ src/main/java/com/example/cyan/dao/RssFeedRepository.java
@@ -0,0 +1,7 @@
+package com.example.cyan.dao;
+
+import com.example.cyan.model.RssFeedModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RssFeedRepository extends CrudRepository<RssFeedModel, Integer> {
+}
